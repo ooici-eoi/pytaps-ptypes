debe1197ba73e7aa41fad068e2fb3e4e1afb6c45
Christopher Mueller <cmueller@asascience.com>
2012-03-02 13:56:42 -0500

    Added additional tests with tags

--- tag_test.py
+++ tag_test.py
@@ -15,23 +15,52 @@ verts=mesh.createVtx(coords)
 vert_set=mesh.createEntSet(False)
 vert_set.add(verts)
 
-data=np.arange(10).astype(np.int32)
+everts=[]
+for x in range(len(verts)-1):
+    everts+=[verts[x],verts[x+1]]
+    
+# Create edges
+edges, status = mesh.createEntArr(iMesh.Topology.line_segment, everts)
+
+# Make vertex data
+vdata=np.arange(10).astype(np.int32)
+# Make multi-dim data (arrays on verts)
 mdata=np.empty([10,10]).astype(np.int32)
 for x in xrange(10):
-    mdata[x]=data.copy()
+    mdata[x]=vdata.copy()
+# Make edge data
+edata=np.arange(100,109).astype(np.int32)
 
-ents_tag=mesh.createTag('values_on_ents', 1, np.int32)
-ents_tag[verts]=data
+# tag values on vertex enties
+ents_tag=mesh.createTag('values_on_verts', 1, np.int32)
+ents_tag[verts]=vdata
 
-mv_ents_tag=mesh.createTag('multi_value_on_ents', data.size, np.int32)
+# tag arrays on vertices
+mv_ents_tag=mesh.createTag('multi_value_on_verts', vdata.size, np.int32)
 mv_ents_tag[verts]=mdata
 
-eset_tag=mesh.createTag('values_on_ent_set', data.size, np.int32)
-eset_tag[vert_set]=data
+# tag data on entity set
+eset_tag=mesh.createTag('values_on_vert_ent_set', vdata.size, np.int32)
+eset_tag[vert_set]=vdata
 
-fl_tag=mesh.createTag('values_on_first_last_ents', 1, np.int32)
-fl_tag[verts[0]] = data[0]
-fl_tag[verts[-1]] = data[-1]
+# tag first and last vertex values
+fl_tag=mesh.createTag('values_on_first_last_verts', 1, np.int32)
+fl_tag[verts[0]] = vdata[0]
+fl_tag[verts[-1]] = vdata[-1]
 
+
+a_tag=mesh.createTag('tag_a', 1, np.int32)
+# tag all vertices
+a_tag[verts] = vdata
+# tag the first and last edges
+a_tag[edges[0]] = edata[0]
+a_tag[edges[-1]] = edata[-1]
+
+b_tag=mesh.createTag('tab_b', 1, np.int32)
+# tag the first and last vertices
+b_tag[verts[0]] = vdata[0]
+b_tag[verts[-1]] = vdata[-1]
+# tag all edges
+b_tag[edges] = edata
 
 mesh.save('tag_test.h5m')
