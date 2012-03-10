2b95cbd3198ac8041e9622f56a6aec7ad7e36d40
Christopher Mueller <cmueller@asascience.com>
2012-03-10 09:16:21 -0500

    Introduced underlying classes and refined Structure
    Added Parameter and BaseIndexing classes
    Moved slicing (__getitem__) from Structure to BaseIndexing
    Assign Parameter instances to map based on tag & cell_dimension
    Assign BaseIndexing instances to .ni (natural indexing) and .gi (grid indexing) fields of each Parameter instance

--- ooici_structure.py
+++ ooici_structure.py
@@ -29,139 +29,111 @@ def make_test_mesh(x,y,z=1):
     # Add data to the entities in each cell_dimension
     for i in range(4):
         ents=mesh.getEntities(type=i)
-        tag=mesh.createTag('%s_data' % i,1,np.int32)
-        m=np.prod([10]*(i+1))
-        dat=np.arange(m,m+len(ents),dtype=np.int32)
-        #        print 'start: %s, end: %s' % (m, m+100)
-        #        dat=rnd.uniform(m,m+100,len(ents)).astype(np.int32)
-        print dat
-        tag[ents]=dat
+        if len(ents) > 0:
+            tag=mesh.createTag('%s_data' % i,1,np.int32)
+            m=np.prod([10]*(i+1))
+            dat=np.arange(m,m+len(ents),dtype=np.int32)
+    #        print 'start: %s, end: %s' % (m, m+100)
+    #        dat=rnd.uniform(m,m+100,len(ents)).astype(np.int32)
+            print dat
+            tag[ents]=dat
 
     return mesh
 
-class Structure(object):
-    def __init__(self, mesh=None, grid_dimensions=None):
-        if grid_dimensions is None: #TODO: Temporary, replace with grid_dim or 'default' statement
-            x=4
-            y=4
-            z=1
-            if z is 1:
-                z1=1
-            else:
-                z1=z-1
 
-            self.gdims=((x,y,z),(x-1,y-1,z),(x-1,y-1,z),(0,))
-        else:
-            self.gdims=grid_dimensions
+class Parameter(object):
+    def __init__(self, parent_structure, tag_hndl, ent_hndls, temporal_shape, grid_shape=None):
+        self.pstruct=parent_structure
+        self.tag_hndl=tag_hndl
+        self.ent_hndls=ent_hndls
 
-        #        self.gdims=((3,1,1),(2,1,1),(0,),(0,))
-        #        self.gdims=((3,3,1),(2,2,1),(2,2,1),(0,))
-        #        self.gdims=((4,4,1),(3,3,1),(3,3,1),(0,))
-        #        self.gdims=((20,30,1),(19,29,1),(19,29,1),(0,))
-        #        self.gdims=((3,3,2),(2,2,1),(2,2,1),(2,2,1))
-        #        self.gdims=((3,3,3),(2,2,2),(2,2,2),(2,2,2))
-        #        self.gdims=((3,3,4),(2,2,3),(2,2,3),(2,2,3))
-        #        self.gdims=((3,4,4),(2,3,3),(2,3,3),(2,3,3))
+        cdim_shp=(len(self.ent_hndls),)
+        if not isinstance(temporal_shape, tuple): temporal_shape = (temporal_shape,)
+        grid_shape=grid_shape or (0,)
+        if not isinstance(grid_shape, tuple): grid_shape = (grid_shape,)
 
+        self.ni = BaseIndexing(self, shape=temporal_shape+cdim_shp) # Natural indexing
+        self.gi = BaseIndexing(self, shape=temporal_shape+grid_shape) # Non-natural (grid) indexing
 
-        self.mesh = mesh or make_test_mesh(self.gdims[0][0], self.gdims[0][1], self.gdims[0][2])
+    @property
+    def name(self):
+        return self.tag_hndl.name
 
-        self.tdim=(1,)
-        cnt=np.bincount(self.mesh.getEntType(self.mesh.getEntities()))
-        if cnt.size < 4:
-            cnt=np.hstack([cnt,[0]*(4-cnt.size)])
-        self.cdims=[(int(i),) for i in cnt]
+    @property
+    def shape(self):
+        return self.ni.shape, self.gi.shape
+
+class BaseIndexing(object):
+    def __init__(self, parent_parameter, shape):
+        self.valid=True
+        self.pparam=parent_parameter
+        self.shape=shape
+        if 0 in shape:
+            self.valid=False
+            return
+
+        self.ent_hndls=self.pparam.ent_hndls.copy().reshape(shape[1:])
 
     def __getitem__(self, index):
+        if not self.valid:
+            raise Exception('Invalid slicing of parameter "%s"' % self.pparam.name)
+
         if not isinstance(index, tuple): index = (index,)
 
-        if not type(index[0]) is tuple:
-            raise Exception("First member must be an tuple of type (int, bool) indicating the (cell_dimension, natural_indexing)")
+        print 'index: %s' % (index,)
 
-        flags=index[0]
-        nat_ind = True
-        cell_dim = 0
-        if len(flags) is 2:
-            if type(flags[1]) is bool:
-                cell_dim = flags[0]
-                nat_ind = flags[1]
-            elif type(flags[0]) is bool:
-                nat_ind = flags[0]
-                cell_dim = flags[1]
-        elif len(flags) is 1:
-            if type(flags[0]) is bool:
-                nat_ind = flags[0]
-            else:
-                cell_dim = flags[0]
-
-        # Remove the 'flag' from the index
-        index=index[1:]
-
-        # Sanity check
-        if cell_dim > 3 or cell_dim < 0:
-            raise Exception('cell dimension must be between 0 & 3 (inclusive)')
-
-        print 'nat_ind: %s, cell_dim: %s' % (nat_ind, cell_dim)
-
-        for slice_ in index:
-            print '%s (%s)' % (slice_,type(slice_))
-
-        # Ignore time for now
-        # TODO: eventually, must determine spatial indices and then concatenate (vstack/hstack, possibly by dimension)
-        # the extracted data across desired times
-        index=index[1:]
-
-        # Get the tag and the array of entity handles for the specified cell_dimension
-        tag=self.mesh.getTagHandle('%s_data' % cell_dim)
-        ents=self.mesh.getEntities(type=cell_dim)
-
-        if not nat_ind:
-            ents=ents.reshape(self.gdims[cell_dim])
-        #            raise Exception('Only natural-indexing is currently supported')
-
-        #        print '>> all_ents: %s' % ents
-        sents=ents[index]
-        #        print '>> slice_ents: %s shp: %s' % (sents,sents.shape)
-        return tag[sents.reshape(np.prod(sents.shape))]
-
-    @property
-    def shape0(self):
-        return self.tdim + self.cdims[0]
-
-    @property
-    def shape1(self):
-        return self.tdim + self.cdims[1]
-
-    @property
-    def shape2(self):
-        return self.tdim + self.cdims[2]
-
-    @property
-    def shape3(self):
-        return self.tdim + self.cdims[3]
-
-    @property
-    def shapeG0(self):
-        return self.tdim + self.gdims[0]
-
-    @property
-    def shapeG1(self):
-        return self.tdim + self.gdims[1]
-
-    @property
-    def shapeG2(self):
-        return self.tdim + self.gdims[2]
-
-    @property
-    def shapeG3(self):
-        return self.tdim + self.gdims[3]
+        # TODO: Temporarily remove time slice (1st member of index)
+        sents=self.ent_hndls[index[1:]]
+        print 'sents: %s' % sents
+        return self.pparam.tag_hndl[sents.reshape(np.prod(sents.shape))].reshape(sents.shape)
 
     def __repr__(self):
-        return 'cdim0shp: %s\n'\
-               'cdim1shp: %s\n'\
-               'cdim2shp: %s\n'\
-               'cdim3shp: %s\n'\
-               'gshp0: %s\n'\
-               'gshp1: %s\n'\
-               'gshp2: %s\n'\
-               'gshp3: %s\n' % (self.shape0,self.shape1,self.shape2,self.shape3,self.shapeG0,self.shapeG1,self.shapeG2,self.shapeG3)
+        return 'entity_handles: %s\nshape: %s' % (self.ent_hndls,self.shape,)
+
+#class GridIndexing(BaseIndexing):
+#    def __init__(self, dims=None):
+#        BaseIndexing.__init__(self, dims=dims)
+#
+#class NaturalIndexing(BaseIndexing):
+#    def __init__(self, dims=None):
+#        BaseIndexing.__init__(self, dims=dims)
+
+
+class Structure(object):
+    def __init__(self, mesh=None, grid_dimensions=None):
+#        if grid_dimensions is None: #TODO: Temporary, replace with grid_dimensions or 'default' statement
+#            x=3
+#            y=3
+#            z=3
+#            if z is 1:
+#                zp=1
+#            else:
+#                zp=z-1
+#
+#            self.gshp=((x,y,z),(0,),(x-1,y-1,zp),(x-1,y-1,zp))
+#        else:
+#            self.gshp=grid_dimensions
+
+#        self.gshp=((3,1,1),(0,),(0,),(0,))
+#        self.gshp=((3,3,1),(0,),(2,2,1),(0,))
+        self.gshp=((4,4,1),(0,),(3,3,1),(0,))
+#        self.gshp=((20,30,1),(0,),(19,29,1),(0,))
+#        self.gshp=((3,3,2),(0,),(0,),(2,2,1))
+#        self.gshp=((3,3,3),(0,),(0,),(2,2,2))
+#        self.gshp=((3,3,4),(0,),(0,),(2,2,3))
+#        self.gshp=((3,4,4),(0,),(0,),(2,3,3))
+
+        self.mesh = mesh or make_test_mesh(self.gshp[0][0], self.gshp[0][1], self.gshp[0][2])
+
+        self.tshp=(1,)
+
+        self.parameters={}
+        for i in range(4):
+            ents=self.mesh.getEntities(type=i)
+            if len(ents) > 0:
+                tags=self.mesh.getAllTags(ents[0])
+                for tag in tags:
+                    self.parameters[tag.name] = Parameter(self, tag, ents, self.tshp, self.gshp[i])
+
+    def __repr__(self):
+        return '# Parameters: %s\n' % len(self.parameters)
